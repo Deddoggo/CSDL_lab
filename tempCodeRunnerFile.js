37394db04c31344eaf7206291f87f43194212e32
Trung Nghĩa Trần <nghia.tt213581@sis.hust.edu.vn>
2023-07-09 11:19:10 +0700

    123

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,13 +1,1 @@
-app.get('/danhsach', (req, res) => {
-  // Thực hiện truy vấn SQL để lấy thông tin về các loại xe
-  const query = 'SELECT * FROM Xe';
-  sql.query(query)
-    .then((result) => {
-      // Gửi kết quả về cho máy khách
-      res.json(result.recordset);
-    })
-    .catch((error) => {
-      console.log('Error executing SQL query:', error);
-      res.status(500).send('Internal Server Error');
-    });
-});
+timxe
